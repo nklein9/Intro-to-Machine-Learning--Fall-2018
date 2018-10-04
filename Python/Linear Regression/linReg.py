16eb74c7b7debcc0858affcccded8e188c500238
Nick Klein <nklein9e@gmail.com>
2018-10-04 10:51:13 -0400

    update

--- Python/Linear Regression/linReg.py
+++ Python/Linear Regression/linReg.py
@@ -6,6 +6,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random as ran
 import decimal
+import math
 
 def linReg(num, m, b):
     ran.seed()                       #changes seed based on system clock
@@ -18,8 +19,8 @@ def linReg(num, m, b):
     
     theta0 = 0                              #initial Y intercept guess of 0
     theta1 = 0                              #initial Slope guess of 0
-    alpha = 0.01                            #Learning incriment
-    jrun = 1                               #how many times cost function runs
+    alpha = 0.0001                            #Learning incriment
+    jrun = 10                               #how many times cost function runs
     
     x = ran.sample(range(100), num)
 
@@ -39,14 +40,17 @@ def linReg(num, m, b):
         
         h = hypothosis(theta0, theta1, x)
         
-    #for i in h:
-    #    print(i)
+    pe = percentError(h, y)
     
-    plt.figure()                        #Adds a figure
-    plt.plot(x, y1, 'r')                #Plots true line
-    plt.plot(x, h, 'g--')                 #plots hypothesis
-    plt.scatter (x, y, c = 'b')         #Scatter of noise values
-    plt.title('')                       #Creates a title for figure
+    tp1 = format(theta1, '.4g')
+    tp0 = format(theta0, '.4g')
+    plt.figure()                                                        #Adds a figure
+    plt.plot(x, y1, 'r', label= 'y={}x+{}'.format(m, b))                #Plots true line
+    plt.plot(x, h, 'g--', label='h={}x+{}'.format(tp1, tp0))            #plots hypothesis
+    plt.scatter (x, y, c = 'b')                                         #Scatter of noise values
+    plt.legend()
+    plt.text(60, .025, r'%Error: {}'.format(pe))
+    plt.title('Linear Regression')                       #Creates a title for figure
     plt.show()                          #displays the figure
     
 def hypothosis(theta0, theta1, x):
@@ -61,7 +65,7 @@ def costFunc(n, x, h, y, a, t0, t1):
     f = []
     
     for i in range(n):
-        co.append(decimal.Decimal((h[i]-y[i])**2))
+        co.append(decimal.Decimal((h[i]-y[i])))
     for i in range (n):
         f.append(x[i] * co[i])
     tn0 = t0 - decimal.Decimal(a) * sum(co)/n
@@ -77,6 +81,16 @@ def costFunc(n, x, h, y, a, t0, t1):
     j = (tn0, tn1)
     
     return j
+
+def percentError(ex, theo):
+    h=[]
+    for i in range(len(ex)):
+        h.append(np.absolute((ex[i] - theo[i])/theo[i]) * 100)
+    
+    a=sum(h)/len(h)
+    pe= format(a, '.4g')
+    
+    return pe
 
 
 linReg(100, 2, 1)
