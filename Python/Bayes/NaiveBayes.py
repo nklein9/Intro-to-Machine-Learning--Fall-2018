223b27be06db6436f9ed02e628ac0d43925260cf
Nicholas Klein <nklein9e@gmail.com>
2018-11-04 16:59:41 -0500

    NaiveBayes update

---
+++ Python/Bayes/NaiveBayes.py
@@ -0,0 +1,14 @@
+#Nicholas Klein
+#Created 10/29/18, Last edit 10/29/18
+#Naive Bayes Theorem
+
+#Import Statements
+import numpy as np
+import matplotlib.pyplot as plt
+import random as ran
+
+
+
+#Run the program
+if __name__ == '__main__':
+    main()
